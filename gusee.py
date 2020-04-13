2ea76393182c6787cbb62e7861545b803def8b40
Jeff Lu <jeff80119@gmail.com>
2020-04-13 13:15:50 +0800

    add count

--- gusee.py
+++ gusee.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1,100)
+c = 0
 while True:
     num = input('請猜數字: ')
     num = int(num)
+    c += 1
     if num < r:
     	print('比答案來的小')
     elif num > r:
     	print('比答案來的大')
     elif num == r:
     	print('終於猜對了！')
+    	print('這是你猜的第', c, '次')
     	break
+    print('這是你猜的第', c, '次')
